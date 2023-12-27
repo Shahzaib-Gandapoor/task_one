ca14fc06fae3706319d8037810e30230362ff014
Shahzaib Gandapoor <Shahzaib.Gandapoor@ambassadors.com>
2023-12-27 19:34:44 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/sidebg.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desk with cup and laptop";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Azure",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      " About Azure. ",
+    url: "https://www.techtarget.com/searchcloudcomputing/definition/Windows-Azure",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development Roadmap",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A roabmap made up for website development enthusiasts.",
+    url: "https://roadmap.sh/",
   },
   {
-    title: "My Resume Site",
+    title: "How To Make Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "An article demonstraitng how a CV or Resume should be made.",
+    url: "https://novoresume.com/career-blog/how-to-write-a-resume-guide",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "How To Make Websites With JavaScript!",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A tutorial which tells us how to code javascript to make websites.",
+    url: "https://www.w3schools.com/howto/howto_website.asp",
   },
 ];
 
